3ec8577ab4427a896299cb56ecbe5b7004c8c06c
RomanBuryy <roman.buriy@gmail.com>
2022-05-17 11:08:44 +0300

    added a component to output users and detailed information about them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'users',
+        component: UsersComponent,
+        children: [
+          {
+            path: 'userDetails/:id', component: UserDetailsComponent
+          }
+        ]},
+      // {path: 'usersDetails/:id', component: UserDetailsComponent},
+    ]),
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
