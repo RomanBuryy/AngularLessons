3be7219039ff846fd711e87367fd529b5f8fcd03
RomanBuryy <roman.buriy@gmail.com>
2022-05-18 19:39:34 +0300

    added a component to output users and detailed information about them

---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+import {UserService} from "../../services/user.service";
+import {IUser} from "../../models/IUser";
+
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+
+  userDetailsObj: IUser;
+
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({id}) => {
+      this.userService.getUserById(id).subscribe(value => this.userDetailsObj = value)
+    });
+  }
+
+}
